cb37645a92750e17bca7f10a059b4340752dfb2f
taylorjames9 <taylorjames9@gmail.com>
2015-02-03 12:53:45 -0800

    Initial Commit to WBY practice

---
+++ WBY-Practice/Assets/Scripts/FindAssignedPosition.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FindAssignedPosition : MonoBehaviour {
+
+
+	public Camera mainCam;
+
+	// Use this for initialization
+	void Start () {
+
+
+		Debug.Log("this is the center position of panel 1: "+ this.transform.position);
+		Debug.Log("trying to convert above position to screenSpace: "+mainCam.WorldToScreenPoint(this.transform.position));
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ WBY-Practice/Assets/Scripts/GoToAssignedPosition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToAssignedPosition : MonoBehaviour {
+
+	private Vector2 myStartPosition;
+	private Vector2 myTargetPosition;
+	public Transform myTarg;
+	private float fracJourney;
+
+	// Use this for initialization
+	void Start () {
+
+		fracJourney = 1.0f;
+		myStartPosition = new Vector2(this.transform.position.x - Random.Range(-55,55), this.transform.position.y);
+		myTargetPosition = GameObject.Find("TopLeft").transform.position;
+
+		Debug.Log ("MyStartPositionIS "+myStartPosition);
+		Debug.Log ("MyTargetPositionIS "+myTargetPosition);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = Vector3.Lerp(myStartPosition, myTarg.position, Time.time);
+	}
+}
