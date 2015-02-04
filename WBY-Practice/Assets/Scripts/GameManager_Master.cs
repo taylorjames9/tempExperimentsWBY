d9fd2d70335f9731598390f7b03fb11b0c038e5b
taylorjames9 <taylorjames9@gmail.com>
2015-02-04 12:51:42 -0800

    First attempt at GameManager

---
+++ WBY-Practice/Assets/Scripts/GameManager_Master.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager_Master : MonoBehaviour {
+
+
+	//PseudoCode
+	//When GameManager_Master (this) loads it tells the frameCountDownBar to go down. 
+	//
+	public static List<string> allLevelNames;
+	public static int levelCounter;
+
+	void Awake(){
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	// Use this for initialization
+	public static void Start () {
+		levelCounter = 0;
+		Debug.Log (" Here is list of all level names");
+		allLevelNames.Add("StoryScene_1");
+		allLevelNames.Add("StoryScene_2");
+		allLevelNames.Add("StoryScene_3");
+		Debug.Log (allLevelNames.ToString());
+	}
+	
+	public static void LoadNextLevel(){
+		Application.LoadLevel(allLevelNames[levelCounter]);
+		levelCounter++;
+	}
+}
---
+++ WBY-Practice/Assets/Scripts/LoadBar.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadBar : MonoBehaviour {
+
+
+	/****PseudoCode
+	At the start of the scene, the loading bar triggers a function called reduce loading bar over time.
+	This function takes two PUBLIC parameters...one that allows you two set a delay...and the other that says how much time it should take the loading bar to reduce to zero. 
+	When the loading bar finishes counting down...we tell the GameManager. (Probably a static variable on GM that says 
+	Scene counter USE PROPERTIES...ie...anytime this variable changes...trigger a new scene to load. 
+	****/
+
+	public float delayToStart;
+	public float timeToFinish;
+
+	private Transform myLoadingBar; 
+	
+	// Use this for initialization
+	void Start () {
+		myLoadingBar = GameObject.Find ("LoadingBar").transform;
+		StartCoroutine(reduceLoadingBarOverTime(0, 10f));
+	}
+
+	public IEnumerator reduceLoadingBarOverTime(float delayStart, float timeFinish){
+		float timeElapse; 
+		float barSizeX; 
+
+		timeElapse = timeFinish;
+
+		while(timeElapse >= 0){
+			timeElapse = timeFinish - Time.timeSinceLevelLoad;
+			barSizeX = myLoadingBar.localScale.x;
+
+			barSizeX = timeElapse;
+			myLoadingBar.localScale = new Vector2(barSizeX, myLoadingBar.localScale.y);
+			yield return 0;
+		}
+		GameManager_Master.LoadNextLevel();
+		myLoadingBar.gameObject.SetActive(false);
+	}
+}
