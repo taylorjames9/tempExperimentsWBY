abc8b8db763e3c91900c4aa343d4128dee85d3db
taylorjames9 <taylorjames9@gmail.com>
2015-02-04 14:01:11 -0800

    levelNums now displaying on countdown screen

---
+++ WBY-Practice/Assets/Scripts/LevelGui.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelGui : MonoBehaviour {
+
+	Text levelNum;
+
+	void Awake(){
+		DontDestroyOnLoad(transform.parent.gameObject);
+	}
+
+	void Start () {
+		levelNum = GetComponent<Text>();
+	}
+	
+	void Update(){
+		//this needs to be done using properties
+		levelNum.text = GameManager_Master.levelCounter.ToString();
+
+	}
+
+
+}
