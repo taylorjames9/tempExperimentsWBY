495279fdd2ad3abf241ed540b29b0d3506ebd62a
taylorjames9 <taylorjames9@gmail.com>
2015-02-03 18:21:17 -0800

    Trying to get a transition object to play

---
+++ WBY-Practice/Assets/Scripts/RandomSlide_PanelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class RandomSlide_PanelManager : MonoBehaviour {
+
+	private float delayEnter;
+	private  List<Transform> allTransformsOfArtInSequence; 
+	private List<Vector2> allDestinations;
+
+	// Use this for initialization
+	void Start () {
+		int i = 0;
+		allTransformsOfArtInSequence = GameObject.Find("tempArtHolder").GetComponentsInChildren<Transform>().OfType<Transform>().ToList();
+		Debug.Log ("Printout of alltransformsOfArt"+allTransformsOfArtInSequence);
+		foreach(Transform art in allTransformsOfArtInSequence){
+			//art.position = Vector2.Lerp(allTransformsOfArtInSequence[i].position, allDestinations[i].position, Time.time);
+			art.gameObject.SetActive (false);
+				//enderer.enabled = false;
+			i++;
+		}
+	}
+
+//	public IEnumerator moveToDesiredLocation(float delay){
+//
+//
+//	}
+}
