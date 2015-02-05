10c4564c4aca4280b702519379ddad3f03ffa4c4
taylorjames9 <taylorjames9@gmail.com>
2015-02-04 16:27:24 -0800

    Two Render textures working

---
+++ WBY-Practice/Assets/Scripts/LightFlickerScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlickerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		light.intensity = Random.Range(0, 8);
+	
+	}
+}
---
+++ WBY-Practice/Assets/Scripts/Light_Tricks.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Light_Tricks : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(lightFlickr());
+	}
+
+
+	public IEnumerator lightFlickr( ){
+		while(true){
+			light.intensity = Random.Range(0, 3);
+			yield return new WaitForSeconds(1.0f);
+		}
+	}
+}
